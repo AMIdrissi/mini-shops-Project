164b864412ef5112965ed123a743f6fedaf0e457
AMIdrissi <amidrissi2002@gmail.com>
2024-03-11 17:39:22 +0000

    built the logo , can be changed easily later on

---
+++ mini-shop-frontv2/src/logo/Logo.jsx
@@ -0,0 +1,18 @@
+function Logo() {
+  return (
+    <div className="m-1">
+      <h1 className="text-5xl font-sans font-bold">
+        <span
+          className="bg-slate-950 font-serif font-bold text-sky-200 px-1.5 m-1 rounded-md border-2
+         border-sky-200 shadow-sm hover:shadow-lg hover:shadow-sky-200/85 ease-in-out duration-300
+         hover:bg-sky-200 hover:text-black hover:border-2 hover:border-black cursor-pointer"
+        >
+          MO
+        </span>
+        shop
+      </h1>
+    </div>
+  );
+}
+
+export default Logo;
