97cb7657b4af75b8a8ddd36191ea3893d867afa0
AMIdrissi <amidrissi2002@gmail.com>
2024-03-11 15:59:49 +0000

    reconstructed the whole repo for the project to work with tailwindcss and vite

---
+++ mini-shop-frontv2/build.bat
@@ -0,0 +1,4 @@
+@echo off
+npm install -D tailwindcss postcss autoprefixer
+npx tailwindcss init -p
+npm run dev
---
+++ mini-shop-frontv2/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+npm install -D tailwindcss postcss autoprefixer
+npx tailwindcss init -p
+npm run dev
