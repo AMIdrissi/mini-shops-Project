8d36a49071059db6d7ae61d00f70398c3044178c
AMIdrissi <amidrissi2002@gmail.com>
2024-03-11 17:42:16 +0000

    built the logo , can be changed easily later on

--- mini-shop-frontv2/src/main.jsx
+++ mini-shop-frontv2/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import Logo from './logo/Logo'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Logo />
   </React.StrictMode>,
 )
