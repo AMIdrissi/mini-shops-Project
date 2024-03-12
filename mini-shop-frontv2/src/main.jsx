c3d6ded3d20b1aae22c5e7fba73551bcca7ee51f
AMIdrissi <amidrissi2002@gmail.com>
2024-03-12 17:26:34 +0000

    i broke the app nice

--- mini-shop-frontv2/src/main.jsx
+++ mini-shop-frontv2/src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import Logo from './logo/Logo'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import NButton from "./buttons/NB_Button";
+import DropDownMenu from "./dropDownMenu/dropDownMenu";
+import Logo from "./logo/Logo";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Logo />
-  </React.StrictMode>,
-)
+     <Logo/>
+  </React.StrictMode>
+);
