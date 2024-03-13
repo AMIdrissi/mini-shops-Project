5af823c58b214a596dfcc8f4d16fe96e06bdb389
AMIdrissi <amidrissi2002@gmail.com>
2024-03-13 16:26:51 +0000

    added some tweaks {sparkle}

--- mini-shop-frontv2/src/logo/Logo.jsx
+++ mini-shop-frontv2/src/logo/Logo.jsx
@@ -3,11 +3,11 @@ import { useEffect } from "react";
 function Logo() {
   return (
     <div className="m-1 ml-3 flex items-center">
-      <h1 className="text-5xl font-sans font-bold text-[#232227]">
+      <h1 className="text-5xl font-sans font-bold text-[#28282a]">
         <span
-          className="bg-[#d4d3cf] font-serif font-bold text-[#4a4c50] px-1.5 m-1 rounded-md border-[3px]
-         border-[#4a4c50] shadow-sm hover:shadow-lg hover:shadow-gray-700/80 ease-in-out duration-300
-         hover:bg-[#4a4c50] hover:text-[#d4d3cf] hover:border-[3px] hover:border-[#4a4c50] cursor-pointer tracking-tighter"
+          className="hover:bg-[#d4d3cf] font-serif font-bold hover:text-[#4a4c50] px-1.5 m-1 rounded-md hover:border-[3px]
+         hover:border-[#4a4c50] hover:shadow-md shadow-lg shadow-gray-700/80 ease-in-out duration-300
+         bg-[#4a4c50] text-[#d4d3cf] border-[3px] border-[#4a4c50] cursor-pointer tracking-tighter"
         >
           アミ
         </span>
