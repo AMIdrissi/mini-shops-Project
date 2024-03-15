83ddcb37fbd650097ce91f3d917f47deb7664426
AMIdrissi <amidrissi2002@gmail.com>
2024-03-15 18:24:46 +0000

    restructure for future hooking with the backend

--- mini-shop-frontv2/src/main.jsx
+++ mini-shop-frontv2/src/main.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import DropDownMenu from "./dropDownMenu/DropDownMenu";
-import NButton from "./buttons/NavBarButton";
-import MainPage from "./MainPage/MainPage";
-import ButtonForMenu from "./buttons/ButtonForMenu";
-import TopbgImg from "./panel/HeaderImage";
-import FirstLayer from "./panel/FoodChoice";
-import TagineIcon from "./icons/TajineIcon";
-import FastFoodIcon from "./icons/FastFoodIcon";
-import DessertIcon from "./icons/DessertIcon";
-import FoodChoiceContainer from "./cardContainers/FoodChoiceContainer";
+import DropDownMenu from "./UI/dropDownMenu/DropDownMenu";
+import NButton from "./UI/buttons/NavBarButton";
+import MainPage from "./UI/MainPage/MainPage";
+import ButtonForMenu from "./UI/buttons/ButtonForMenu";
+import TopbgImg from "./UI/panel/HeaderImage";
+import FirstLayer from "./UI/panel/FoodChoice";
+import TagineIcon from "./UI/icons/TajineIcon";
+import FastFoodIcon from "./UI/icons/FastFoodIcon";
+import DessertIcon from "./UI/icons/DessertIcon";
+import FoodChoiceContainer from "./UI/cardContainers/FoodChoiceContainer";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
